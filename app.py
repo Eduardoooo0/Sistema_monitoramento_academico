72dce2338a5f5838fd5b55ae64f38505130b8b55
Eduardoooo0 <el957733@gmail.com>
2025-01-16 10:12:26 -0300

    adicionando lógica do flask-sqlalchemy

--- app.py
+++ app.py
@@ -1,6 +1,22 @@
 from flask import Flask, render_template, request, url_for, redirect
+from flask_sqlalchemy import SQLAlchemy
+from models import db
+from models.tabelas import Alunos
+import os
+from dotenv import load_dotenv
 
-app = Flask(__name__)
+load_dotenv()
+
+app  = Flask(__name__)
+
+app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv('URL')
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+db.init_app(app)
+
+
+with app.app_context():
+    db.create_all()
 
 @app.route('/')
 def index():
